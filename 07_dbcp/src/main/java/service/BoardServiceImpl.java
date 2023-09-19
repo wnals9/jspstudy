6bb257ca0c981b93a00b4d01f1e6afe7478c768d
wnals9 <wnals16@naver.com>
2023-09-19 17:43:01 +0900

    20230919

--- 07_dbcp/src/main/java/service/BoardServiceImpl.java
+++ 07_dbcp/src/main/java/service/BoardServiceImpl.java
@@ -4,9 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import common.ActionForward;
 import domain.BoardDto;
+import repository.BoardDao;
 
 public class BoardServiceImpl implements BoardService {
 
+  // 모든 서비스가 공동으로 사용하는 BoardDao 객체 가져오기
+  private BoardDao dao = BoardDao.getDao();
+  
   @Override
   public ActionForward register(HttpServletRequest request) {
     
@@ -19,10 +23,20 @@ public class BoardServiceImpl implements BoardService {
                       .title(title)
                       .content(content)
                       .build();
+
+    // BoardDao의 register 메소드 호출
+    int registerResult = dao.register(dto);
     
-    System.out.println(dto);
+    // 등록 성공(registerResult == 1), 등록 실패(registerResult == 0)
+    String path = null;
+    if(registerResult == 1) {
+      path = "/board/list.jsp";
+    } else if(registerResult == 0) {
+      path = "/index.jsp";
+    }
     
-    return null;
+    // 어디로 어떻게 이동하는지 반환 (insert 수행 후에는 반드시 redirect 이동한다.)
+    return new ActionForward(path, true);
     
   }
 
